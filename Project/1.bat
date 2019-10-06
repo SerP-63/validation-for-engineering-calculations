e9d685bfd15cc32f527aec81f81588290320989d
SerP-63 <ramblers007@rambler.ru>
2019-10-06 14:20:34 +0200

    Реализация LoginPage

--- Project/1.bat
+++ Project/1.bat
@@ -1,2 +1,4 @@
 chcp 1251 > nul
+cls
+
 pytest -v --tb=line --language=en test_main_page.py
--- Project/test_main_page.py
+++ Project/test_main_page.py
@@ -1,5 +1,10 @@
+from pages.main_page import MainPage
+from pages.login_page import LoginPage
+from pages.locators import LoginPageLocators
+
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+    page = LoginPage(browser, LoginPageLocators.LOGIN_PAGE_URL)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    #page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
+    #page.should_be_login_link()
+    page.should_be_login_page()
