ea93a483cd54d0bf4225d98c04c7b398cec9b519
SerP-63 <ramblers007@rambler.ru>
2019-10-09 20:42:41 +0200

    Переходы между страницами

--- Project/test_main_page.py
+++ Project/test_main_page.py
@@ -3,8 +3,8 @@ from pages.login_page import LoginPage
 from pages.locators import LoginPageLocators
 
 def test_guest_can_go_to_login_page(browser):
-    page = LoginPage(browser, LoginPageLocators.LOGIN_PAGE_URL)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    page.open()                      # открываем страницу
-    #page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
-    #page.should_be_login_link()
-    page.should_be_login_page()
+    page = MainPage(browser, LoginPageLocators.LOGIN_PAGE_URL)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
